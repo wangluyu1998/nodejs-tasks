8ce222d8727b96c48099e3178c0073cf38f1e740
wangluyu1998 <2602649277@qq.com>
2019-11-12 14:54:08 +0800

    update

--- task05-Blog/routes/index.js
+++ task05-Blog/routes/index.js
@@ -5,17 +5,16 @@ var fs = require('fs');
 var dataObj = fs.readFileSync('./data.json','utf8');
 // console.log(dataObj);
 var obj = JSON.parse(dataObj);
-console.log(obj);
+// console.log(obj);
 // console.log(obj.users[0].username);
 // console.log(obj.users[0].password);
-// console.log(obj.chapterList);
 
 router.get('/', function(req, res,next) {
   res.render('login',{title:'login'});
 });
 
 router.get('/list',function(req,res,next){
-  res.render('list',{title:'list'})
+  res.render('list',{title:'list',data:obj})
 });
 
 router.post('/',function(req,res,next){
@@ -24,7 +23,7 @@ router.post('/',function(req,res,next){
   if(req.body.username === obj.users[0].username && req.body.pwd === obj.users[0].password){
     res.render('list',{title:'list',data:obj}); 
   }else{
-    res.send('用户名或密码错误，请重新输入！');
+    res.send('账号或密码错误，请重新输入！');
   }
 });
 
